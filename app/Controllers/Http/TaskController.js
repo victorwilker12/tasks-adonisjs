44453cd085d90fecf63ca17d51c5236a1411ed21
Victor Wilker <wilkersantos125@gmail.com>
2020-06-05 23:27:01 -0300

    create views and controllers

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -1,8 +1,18 @@
 'use strict'
 
 class TaskController {
-  index(){
-    return 'Task'
+  index({view}){
+
+    const tasks = [
+      {title:'Task one', body: 'This is task one' },
+      {title:'Task two', body: 'This is task two' },
+    ]
+
+
+    return view.render('task',{
+      title: 'Your Tasks',
+      tasks: tasks
+    })
   }
 }
 
